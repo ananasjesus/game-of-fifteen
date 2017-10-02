9b0f16d0a64d3f020fa803c651970616c0ed2997
Mona Lisa <mona@lisa.io>
2017-10-02 21:32:06 +0300

    FieldHelperTest create

---
+++ .idea/test/helpers/FieldHelperTest.java
@@ -0,0 +1,20 @@
+package helpers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import model.*;
+
+public class FieldHelperTest {
+    @Test
+    public void getClassicField() throws Exception {
+
+        final Field testField = FieldHelper.getClassicField();
+        final int expextedValue = 10;
+        final int expectedId = 10;
+        final int realValue = (int) testField.getFigure(expextedValue).getValue();
+        assertEquals(expextedValue, realValue);
+    }
+
+}
