9f4e8b960b7382bc17eb37e6bcaf87622ebc7cea
Mona Lisa <mona@lisa.io>
2017-10-03 00:17:56 +0300

    Game add constructor

--- .idea/src/model/Game.java
+++ .idea/src/model/Game.java
@@ -1,4 +1,18 @@
 package model;
 
 public class Game {
+    private final String gameName;
+
+    private final String playerName;
+
+    private final Field field;
+
+    public Game(String gameName, String playerName, Field field) {
+        this.gameName = gameName;
+        this.playerName = playerName;
+        this.field = field;
+    }
+
+
+
 }
