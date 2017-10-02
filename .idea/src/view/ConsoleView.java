b22c643143bfee827e5736aab2ac1436f9e962e5
Mona Lisa <mona@lisa.io>
2017-10-03 02:17:17 +0300

    ConsoleView add clear

--- .idea/src/view/ConsoleView.java
+++ .idea/src/view/ConsoleView.java
@@ -2,6 +2,22 @@ package view;
 
 import controller.*;
 
+import java.io.IOException;
+
 public class ConsoleView {
 
+    public static void clear() {
+
+        Runtime runtime = Runtime.getRuntime();
+
+        if (System.getProperty("os.name").contains("Windows"))
+            try {
+            runtime.exec("cls");
+            } catch (IOException e) {}
+        else
+            try {
+                runtime.exec("clear");
+            } catch (IOException e) {}
+
+    }
 }
