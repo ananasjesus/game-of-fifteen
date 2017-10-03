3a0b3e122aa36ebcb2b855be4b756efd8635a73f
Mona Lisa <mona@lisa.io>
2017-10-03 13:20:38 +0300

    WinController improved

--- .idea/src/controller/WinController.java
+++ .idea/src/controller/WinController.java
@@ -12,4 +12,16 @@ public class WinController {
 
         return true;
     }
+
+    public static boolean isWin(Game game) {
+        final Field field = game.getField();
+        final int fieldSize = field.getSize();
+
+        for (int i = 1; i < fieldSize; i++)
+            if (field.getFigure(i).getId() != i)
+                return false;
+
+        return true;
+    }
+
 }
